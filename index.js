c130c22e12494549482fed471764e919d45b07c1
dwalley606 <dwalley606@gmail.com>
2024-04-16 11:17:16 -0400

    Added questions for inquirer, added README.md, set writeFile function to target readme

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Enter a description of your project'
+    },
+    {
+        type: 'input',
+        name: 'Table of Contents',
+        message: 'Enter a table of contents for your project'
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'Describe how to install your project'
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Describe how to use your project'
+    },
+    {
+        type: 'input',
+        name: 'License',
+        message: 'Enter the license for your project'
+    },
+    {
+        type: 'input',
+        name: 'Contributing',
+        message: 'Enter any other contributors to your project'
+    },
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'Describe any tests you ran on the project'
+    },
+    {
+        type: 'input',
+        name: 'Questions',
+        message: 'Enter your Github username'
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(README.md, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
